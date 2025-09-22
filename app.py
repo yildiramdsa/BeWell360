c03d64918367a1ea2b98a18dee7800c1c81985c4
Alina Yildir <ms.alina.yildir@gmail.com>
2025-09-22 02:34:58 -0600

    Powered by

--- app.py
+++ app.py
@@ -15,15 +15,18 @@ if not os.path.exists(DATA_DIR):
 
 # ---------------- Footer ----------------
 st.markdown("<hr>", unsafe_allow_html=True)
+
 col1, col2, col3 = st.columns([1, 2, 1])
+
 with col2:
+    st.markdown(
+        "<div style='text-align: center;'>"
+        "<span style='margin-right:10px;'>Powered by</span>"
+        "</div>",
+        unsafe_allow_html=True
+    )
     st.image("images/Oranlytix-lg.svg", width=150)
     st.markdown(
-        """
-        <div style='text-align: center; margin-top:5px;'>
-            <span style='font-weight:bold;'>Powered by Oranlytix</span><br>
-            <span style='font-size:12px; color:gray;'>© 2025 BeWell360. All rights reserved.</span>
-        </div>
-        """,
+        "<div style='text-align:center; font-size:12px; color:gray;'>© 2025 BeWell360. All rights reserved.</div>",
         unsafe_allow_html=True
     )
