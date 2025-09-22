9c2d482338e6cb0a3b56d7114d2f52f14601818e
Alina Yildir <ms.alina.yildir@gmail.com>
2025-09-22 03:45:52 -0600

    Daily Log Pages

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ with open("images/Oranlytix-lg.svg", "rb") as f:
     encoded_svg = base64.b64encode(svg_bytes).decode()
 footer_html = f"""
 <div style="text-align:center;">
-    <span style="font-weight:bold; font-size:12px;">Powered by</span>
+    <span style="font-size:12px;">Powered by</span>
     <img src="data:image/svg+xml;base64,{encoded_svg}" width="100">
     <div style="font-size:12px; color:gray; margin-top:5px;">© 2025 BeWell360. All rights reserved.</div>
 </div>
