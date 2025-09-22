238f8b03e4dd2ebfda088747e2ebaa147385ebef
Alina Yildir <ms.alina.yildir@gmail.com>
2025-09-22 02:44:37 -0600

    Powered by

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ if not os.path.exists(DATA_DIR):
 
 # ---------------- Footer ----------------
 st.markdown("<hr>", unsafe_allow_html=True)
-st.markdown("<div style='text-align:center;'>", unsafe_allow_html=True)
-st.markdown("<span style='font-weight:bold; font-size:12px;'>Powered by</span>", unsafe_allow_html=True)
-st.image("images/Oranlytix-lg.svg", width=100)
-st.markdown("<div style='font-size:12px; color:gray;'>© 2025 BeWell360. All rights reserved.</div>", unsafe_allow_html=True)
-st.markdown("</div>", unsafe_allow_html=True)
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.markdown("<span style='font-weight:bold; font-size:12px;'>Powered by</span>", unsafe_allow_html=True)
+    st.image("images/Oranlytix-lg.png", width=100)
+    st.markdown("<div style='font-size:12px; color:gray;'>© 2025 BeWell360. All rights reserved.</div>", unsafe_allow_html=True)
 
