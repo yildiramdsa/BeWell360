58aa88d476445a87d8efc433fbe07746f5b50892
Alina Yildir <ms.alina.yildir@gmail.com>
2025-09-22 03:46:39 -0600

    Daily Log Pages

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ footer_html = f"""
 <div style="text-align:center;">
     <span style="font-size:12px;">Powered by</span>
     <img src="data:image/svg+xml;base64,{encoded_svg}" width="100">
-    <div style="font-size:12px; color:gray; margin-top:5px;">© 2025 BeWell360. All rights reserved.</div>
+    <div style="font-size:12px; margin-top:5px;">© 2025 BeWell360. All rights reserved.</div>
 </div>
 """
 st.markdown(footer_html, unsafe_allow_html=True)
