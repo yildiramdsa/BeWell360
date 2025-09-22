f0f1c7a39dce948f2c3990c0bca8cb2ff0ed474b
Alina Yildir <ms.alina.yildir@gmail.com>
2025-09-22 03:44:31 -0600

    Daily Log Pages

--- app.py
+++ app.py
@@ -13,26 +13,11 @@ with open("images/BeWell360-lg.svg", "rb") as f:
 st.markdown(
     f"""
     <div style="text-align:center;">
-        <img src="data:image/svg+xml;base64,{encoded_svg}" width="350">
+        <img src="data:image/svg+xml;base64,{encoded_svg}" width="300">
     </div>
     """,
     unsafe_allow_html=True
 )
-
-# ---------------- Logos ----------------
-# Large logo at top of main page
-st.markdown(
-    f"""
-    <div style="text-align:center;">
-        <img src="images/BeWell360-lg.svg" width="350">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
-# Small logo in sidebar
-with st.sidebar:
-    st.image("images/BeWell360-sm.svg", width=50)
 
 # ---------------- Daily Log Pages ----------------
 nutrition_and_hydration = st.Page(
